944859d4549e4aeb690cda8596778b80658e8447
Jason Gellatly <jason.gellatly@gmail.com>
2026-06-13 21:20:17 +1000

    Convert handler to its own struct with handlers. This will allow using struct level attributes like loggers

--- pkg/webserver/handler.go
+++ pkg/webserver/handler.go
@@ -6,13 +6,27 @@ import (
 	"github.com/xyzjace/terraplane/pkg/log"
 )
 
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+type handler struct {
+	logger log.Logger
+	mux    *http.ServeMux
 }
 
 func NewHandler(logger log.Logger) http.Handler {
-	x := http.NewServeMux()
-	x.HandleFunc("/health", healthCheckHandler)
-	return x
+	h := &handler{
+		logger: logger,
+		mux:    http.NewServeMux(),
+	}
+
+	h.mux.HandleFunc("GET /health", h.healthCheck)
+
+	return h
+}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.mux.ServeHTTP(w, r)
+}
+
+func (h *handler) healthCheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
