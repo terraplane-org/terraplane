ae320f38b17beb002c1e043098ac79cf3250c172
Jason Gellatly <jason.gellatly@gmail.com>
2026-07-16 21:14:14 +1000

    Allow a tf var for string length

--- terraform/environments/staging/ap-southeast-2/foundation/main.tf
+++ terraform/environments/staging/ap-southeast-2/foundation/main.tf
@@ -1,4 +1,10 @@
+variable "random_length" {
+  type        = number
+  description = "Length of the random_string resource. Override with TF_VAR_random_length."
+  default     = 16
+}
+
 resource "random_string" "random" {
-  length           = 16
-  special          = true
+  length  = var.random_length
+  special = true
 }
